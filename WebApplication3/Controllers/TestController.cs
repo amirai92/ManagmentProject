81cf46a79cd0dd41db6431ae41c0f06d55f45eb8
Amir Aizin <amiraizin20@gmail.com>
2019-03-23 17:58:37 +0200

    Created Test controller and View of Test controller

---
+++ WebApplication3/Controllers/TestController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThisAbility.Dal;
+using ThisAbility.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AutoLoad()
+        {
+            Employee emp = new Employee {
+                FirstName="Test",
+                LastName="Test",
+                UserName="TestName",
+                Password="123"
+            };
+            return View(emp);
+
+        }
+        public ActionResult CheckLogIn(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                DataLayer dal = new DataLayer();
+                dal.employees.Add(emp);
+                dal.SaveChanges();
+                Console.WriteLine("Succesful logged in and saved in DB\n");
+                return View("loggedin", emp);
+            }
+            else
+            {
+                Console.WriteLine("Couldnt add the user,try agian\n");
+                return View("AutoLoad");
+            }
+        }
+    }
+}
