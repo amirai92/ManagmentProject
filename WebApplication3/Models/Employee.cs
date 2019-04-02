ef45044e5570f189d5b969ec94f35e48f4eeb3c6
Sali Dalal <Sali Dalal@DESKTOP-NGBCAOE>
2019-04-02 19:43:11 +0300

    half cv

--- WebApplication3/Models/Employee.cs
+++ WebApplication3/Models/Employee.cs
@@ -8,19 +8,33 @@ namespace WebApplication3.Models
 {
     public class Employee
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
 
         [Key]
         [Required]
         [StringLength(15, MinimumLength = 3, ErrorMessage = "User Name must be 3 to 15 characters")]
-        public string UserName { get; set; }
+        private string UserName { get; set; }
 
 
         [Required]
         [StringLength(15, MinimumLength = 3, ErrorMessage = "Password must be 3 to 15 characters")]
-        public string Password { get; set; }
+        private string Password { get; set; }
 
+        public CV cv { get; set; }
+
+        Employee(string UserName,string Password, string FirstName, string LastName)
+        {
+            this.UserName = UserName;
+            //encr?
+            this.Password = Password;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            cv = new CV(FirstName,LastName);
+
+        }
 
     }
 }
---
+++ WebApplication3/Models/VolunteerHobby.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class VolunteerHobby
+    {
+
+        [Required(ErrorMessage = "Please enter Language Name")]
+        public string LanguageName { get; set; }
+        
+    }
+}
