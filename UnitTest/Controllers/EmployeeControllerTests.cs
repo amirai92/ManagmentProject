2058559671374719775e1f5120f9f815ae5c8207
Amir Aizin <amiraizin20@gmail.com>
2019-05-27 18:29:48 +0300

    Test's Of Employee , Employer .Manager Controllers.

---
+++ UnitTest/Controllers/EmployeeControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication3.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers.Tests
+{
+    [TestClass()]
+    public class EmployeeControllerTests
+    {
+
+        [TestMethod()]
+        public void WatchingContentTest()
+        {
+            EmployeeController con = new EmployeeController();
+            ViewResult res = con.WatchingContent() as ViewResult;
+            Assert.IsNotNull(res);
+
+        }
+
+        [TestMethod()]
+        public void EmployeeLoginTest()
+        {
+            EmployeeController con = new EmployeeController();
+            ViewResult res = con.EmployeeLogin() as ViewResult;
+            Assert.IsNotNull(res);
+        }
+    }
+}
